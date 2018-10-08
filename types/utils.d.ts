c6a4dfc64cf9f33251bce608121c1f7402423e24
Valentina Vandro <vale@Valentinas-MBP.station>
2018-10-08 11:47:18 +0200

    Added github pages

--- types/utils.d.ts
+++
@@ -1,8 +0,0 @@
-export declare function normalizeString(str: string): string;
-export declare function daysInMonth(m: number, y: number): 29 | 28 | 30 | 31;
-export declare function isValidDate(d: number, m: number, y: number): boolean;
-export declare function getValidDate(d: number, m: number, y: number): Date;
-export declare function extractVowels(str: string): string;
-export declare function extractConsonants(str: string): string;
-export declare function pad(n: number, size?: number): string;
-export declare function birthplaceFields(selector: string): void;
